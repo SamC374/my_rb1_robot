31d35076cbbd22f0e7df6264be5cc145b33c3ef2
samc374 <samcly374@gmail.com>
2024-09-11 10:13:48 +0000

    Updated for first evaluation

--- my_rb1_ros/src/rotate_service.cpp
+++ my_rb1_ros/src/rotate_service.cpp
@@ -6,7 +6,6 @@
 #include <my_custom_srv_msg_pkg/Rotate.h>
 #include <ros/ros.h>
 #include <tf/tf.h>
-#define PI 3.14159
 
 class RobotRotator {
 
@@ -16,9 +15,9 @@ private:
   ros::ServiceServer rotate_service;
   float target_yaw, curr_yaw;
   bool reached_target_yaw = false;
-  bool begin_rotating = false;
   geometry_msgs::Twist twist_msg;
-  float kp = 0.5;
+  float kp = 1.0;
+  std::string rotate_service_name = "/rotate_robot";
 
 public:
   RobotRotator(ros::NodeHandle *nh) {
@@ -29,7 +28,8 @@ public:
     odom_sub = nh->subscribe("/odom", 10, &RobotRotator::odom_callback, this);
 
     rotate_service = nh->advertiseService(
-        "/rotate_robot", &RobotRotator::rotate_robot_callback, this);
+        rotate_service_name, &RobotRotator::rotate_robot_callback, this);
+    ROS_INFO("%s Service Ready", rotate_service_name.c_str());
   }
 
   void odom_callback(const nav_msgs::Odometry &msg) {
@@ -46,8 +46,6 @@ public:
     double roll, pitch, yaw;
     mat.getRPY(roll, pitch, yaw);
     curr_yaw = yaw;
-    if (begin_rotating)
-      rotate_robot();
     // ROS_INFO("curr_yaw:%.2f", curr_yaw);
   }
 
@@ -55,22 +53,26 @@ public:
                              my_custom_srv_msg_pkg::Rotate::Response &res) {
     int input_degrees_int = req.degrees;
     float input_degrees_float = static_cast<float>(input_degrees_int);
-    if (abs(input_degrees_int) > 0) {
+    ROS_INFO("%s Service Requested: Rotating robot by %.2f degrees.",
+             input_degrees_float,
+             rotate_service_name.c_str()); // Service Requested message
 
-      target_yaw =
-          normalize_angle(input_degrees_float * M_PI / 180.0 + curr_yaw);
-      ROS_INFO("target_yaw:%.2f, abs(input_degrees_int):%i", target_yaw,
-               abs(input_degrees_int));
-      begin_rotating = true;
-    } else
-      begin_rotating = false;
+    target_yaw = normalize_angle(input_degrees_float * M_PI / 180.0 + curr_yaw);
+    reached_target_yaw = false;
+
+    //   ROS_INFO("target_yaw:%.2f, abs(input_degrees_int):%i", target_yaw,
+    //            abs(input_degrees_int));
+
     ros::Rate rate(10);
     while (!reached_target_yaw && ros::ok()) {
+      rotate_robot();
       rate.sleep();
       ros::spinOnce();
     }
     res.result = "Rotation completed successfully.";
-
+    ROS_INFO("%s Service Completed: Rotation complete.",
+             rotate_service_name.c_str());
+    ROS_INFO("%s Service Ready", rotate_service_name.c_str());
     return true;
   }
 
@@ -85,9 +87,9 @@ public:
 
   void rotate_robot() {
     float yaw_diff = normalize_angle(target_yaw - curr_yaw);
-    if (abs(yaw_diff) > 0.001) {
-      ROS_INFO("target_yaw:%.2f, curr_yaw:%.2f, yaw_diff:%.2f", target_yaw,
-               curr_yaw, yaw_diff);
+    if (abs(yaw_diff) > 0.1 * M_PI / 180.0) {
+      //   ROS_INFO("target_yaw:%.2f, curr_yaw:%.2f, yaw_diff:%.2f", target_yaw,
+      //            curr_yaw, yaw_diff);
       twist_msg.angular.z = yaw_diff * kp;
       twist_pub.publish(twist_msg);
       reached_target_yaw = false;
